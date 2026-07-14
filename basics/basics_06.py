1835c6b38e10da06c2e60d50e53cd6ad1f1d3685
2k25csaiml2513533-gif <2k25aiml2513533@gmail.com>
2026-07-14 16:02:58 +0530

    sum of three num

---
+++ basics/basics_06.py
@@ -0,0 +1,10 @@
+x=int(input("enter first number"))
+y=int(input("enter second number"))
+z=int(input("enter third number"))
+print(x+y+z)
+# int main(){
+#     int a,b,c;
+#     scanf("%d%d%d",&a,&b,&c);
+#     printf(a+b+c);
+#     return 0;
+# }
