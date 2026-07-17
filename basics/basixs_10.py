2b09921b9f12a7d7669ad7be1a66c0e149cb0f22
2k25csaiml2513533-gif <2k25aiml2513533@gmail.com>
2026-07-17 21:52:46 +0530

    basics

---
+++ basics/basixs_10.py
@@ -0,0 +1,13 @@
+import math
+# WAP to calculate and display the interest on a loan amount rupees
+# p=int(input("enter principle amount"))
+# r=int(input("enter time interval"))
+# t=int(input("enter time duration in years"))
+# interest=(p*r*t)/100
+# print(interest)
+# Area of Circle
+# r=int(input("enter radius of a circle"))
+# print(3.14*r*r)
+# Reverse 5 digit number without loop
+
+
---
+++ basics/bassics_09.py
@@ -0,0 +1,9 @@
+x=int(input("enter a number"))
+y=int(input("enter a number"))
+z=int(input("enter a number"))
+if x>y and x>z : 
+    print(f"Greater element is {x}")
+elif y>x and y>z : 
+    print(f"Greater element is {y}")
+else: 
+    print(f"Greater element is {z}")
