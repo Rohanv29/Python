79da4db2a4e727a28c581f5a709014325dfb7542
2k25csaiml2513533-gif <2k25aiml2513533@gmail.com>
2026-07-24 14:13:20 +0530

    loops

---
+++ basics/basics_10.py
@@ -0,0 +1,25 @@
+#calculate sum of num from 1 to 10;
+# sum=0;
+# for i in range(1,11):
+#     sum+=i
+# print(sum)    
+
+# dict ={'a':10,'b':20,'c':30}
+# for i in dict:
+#     print(dict[i])
+# i=1;
+# sum=0
+# while i<11 :
+#     sum+=i
+#     i+=1
+# print(sum)
+#wap that print all the numbers from 0 to 6 except 3 and 6
+i=0
+while i<=6:
+    if i == 3 or i==6:
+        i+=1
+        continue;
+    else : 
+        print(i)
+        i+=1
+        
--- functions/fun_04.py
+++ functions/fun_04.py
@@ -1,25 +1,12 @@
-def is_prime(n):
 
-   for i in range(2, n):
+# n=13
+# for i in range(2, n):
 
-      if n % i == 0:
+#    if n % i == 0:
+#       print("not prime")
+   
 
-         return False
-
-   return True
+# print(" is prime")
 
  
 
-def generate_twins(start, end):
-
-   for i in range(start, end):
-
-      j = i + 2
-
-      if(is_prime(i) and is_prime(j)):
-
-         print('(',i,',',j,')', end=',')
-
-#Main Program
-
-generate_twins(2, 100)
