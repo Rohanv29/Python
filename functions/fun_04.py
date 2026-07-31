bc557862fdaa1d09b872738d5b75c28a0511fcd6
2k25csaiml2513533-gif <2k25aiml2513533@gmail.com>
2026-07-31 22:09:23 +0530

    split and rsplit in python

--- functions/fun_04.py
+++ functions/fun_04.py
@@ -9,4 +9,32 @@
 # print(" is prime")
 
  
-
+s="Programming"
+# print(s[9])
+# print(s[-1])
+# print(s[-4])mn 
+# print(s[::-1])
+# c=0
+# for char in s:
+#     c=c+1
+# print(c)
+# c=0
+# for char in s:
+#     print(char,c)
+#     c=c+1
+    
+# a="python"
+# b="programming"
+# print(a+b)
+# print(a+' '+b)
+# print(a*3)
+# print('py'in a)
+# print('py'not in a)
+# # wap to accept first name and second name and print the full name
+# x=input("enter your first name")
+# y=input("enter your second name")
+# print(x+' '+y) 
+for i in range(0,6):
+    for j in range(0,i):
+        print("*",end="")
+    print("\n")
---
+++ questions/que2.py
@@ -0,0 +1,13 @@
+# str="python  is programming is powerful python  is programming is powerful"
+# p=str.index("is")
+# print(p)
+# s=str.rindex("is")
+# print(s)
+# print(str.split(" "))
+# # print(str.rsplit(" ",1))    
+nm=int(input("enter ur age"))
+name=input("enter your name")
+if(name.isalpha()):
+    print("valid name")
+if(nm.is_integer()):
+    print("valid age")
