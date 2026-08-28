5a0ddf85c24a993d4fd4683658c891e48ea38a7c
2k25csaiml2513533-gif <2k25aiml2513533@gmail.com>
2026-08-28 23:08:02 +0530

    imp files

---
+++ rough.py
@@ -0,0 +1,28 @@
+# import random
+# number=random.randint(1,100)
+# while(True):
+#     guess=int(input("enter number to start the game"))
+#     if(guess<number):
+#         print("Wrong anss increase ur number")
+#     elif(guess>number):
+#         print("Wrong anss decrease ur number")
+#     else:
+#         print("you guessed right")
+#         break
+# import keyword
+# print(keyword.kwlist)
+# print(len(keyword.kwlist))
+# for i in range(1,6):
+#     for j in range(i):
+#         print(chr(97+j),end=" ")
+#     print(" ")
+s="Hello World!! 1234"
+alp=0
+let=0
+for i in s:
+    if(i.isalpha() ):
+        alp+=1
+    elif( i.isdigit()):
+        let+=1
+print(let)
+print(alp)
