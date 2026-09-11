0e9133521f730ede23946d8fe16bbe34593a3c8b
2k25csaiml2513533-gif <2k25aiml2513533@gmail.com>
2026-09-11 14:46:33 +0530

    Dictionary

---
+++ Dictionaries/dict_01.py
@@ -0,0 +1,34 @@
+# student = {
+#     "name": "John",
+#     "age": 20,
+#     "course": "Python"
+# }
+
+# print(student)
+# print(type(student))
+# student = dict(
+#     name="Rohan",
+#     age=18,
+#     course="Python"
+# )
+
+# print(student)
+# student.keys()    # keys
+# student.values()  # values
+# student.items()   # key + value
+
+# print(student.popitem())
+
+# print(student.pop("age"))
+# print(student)
+
+
+# s = "hello"
+# freq = {}
+# for ch in s:
+#     if ch in freq:
+#         freq[ch]+=1
+#     else:
+#         freq[ch]=1
+# print(freq)
+
---
+++ Dictionaries/dict_02.py
@@ -0,0 +1,32 @@
+# create a dict of numbers and their squares 
+# d= {x:x**2 for x in range(1,6)}
+# print("Dictionary of numbers and their squares",d)
+
+
+# wap in python to convert a list of words in word leength of ddictionary using dictionary comprehension
+# wds=["Rohan","Rachit","Cartikey","IShaan"]
+# words_length={word: len(word) for word in wds}
+# print("Dictionaries of words and their lengths ", words_length)
+
+#nested dictionary
+nested_dict = {
+    "person1": {
+        "name": "Ishawn",
+        "age": 30,
+        "city": "New York"
+    },
+    "person2": {
+        "name": "Mohan",
+        "age": 25,
+        "city": "Los Angeles"
+    },
+    "person3":{
+        "name": "Ravi",
+        "age": 28,
+        "city": "Chicago"
+    }
+}
+print(nested_dict)
+
+
+
