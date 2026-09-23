587f719388ccacc78389266d1698078042ee1be3
2k25csaiml2513533-gif <2k25aiml2513533@gmail.com>
2026-09-23 16:35:01 +0530

    function in python

---
+++ functions/fun_06.py
@@ -0,0 +1,30 @@
+# def display():
+#     print("Hello World")
+# display()
+# def square(num):
+#     return num*num;
+# print(square(2))
+
+# def add(a,b):
+#     print(a+b)
+# add(2,3)
+
+# def display(name,message="Good Afternoon"):
+#     print(message,name)
+# display("Rohan","good morning")
+
+# def total(*args):
+#     print("arguments",args)
+#     return sum(args)
+# print(total(10,20))
+
+# def dis_student(name,**kwargs):
+#     print(name)
+#     for key,value in kwargs.items():
+#         print(key ,":",value)
+# dis_student("alice",age=20,branch="Cs-aiml",cgpa=8.5)
+
+# def display(*args,**kwargs):
+#     print("Positional Arguments :",args)
+# display(10,20,30)
+
